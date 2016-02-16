f7b0e7db0165adabae081ccb4a708ccba5af4dc7
abc810221 <abc810221@gmail.com>
2016-02-15 23:37:33 -0800

    deployedit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var promise = require('promise');
 var cors = require('cors');
 
 var app = express();
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/client'));
 app.use(cors());
 
 var port = Number(process.env.PORT) || 3000
